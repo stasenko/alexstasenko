1e549597b5933d47846b920b021fd4e8c1afdcf9
Aleksandr Stasenko <oleksandr.stasenko@gmail.com>
2018-11-23 16:43:35 +0300

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,18 @@
+<?php
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$email_subject = "New form submission";
+
+	$email_body = "User Name: $name.\n".
+				"User Email: $visitor_email.\n".
+				"User Message: $message.\n";
+
+	$to = "oleksandr.stasenko@gmail.com";
+
+	mail($to,$email_subject,$email_body);
+
+	header('Location: thanks.html');
+exit;
+?>
